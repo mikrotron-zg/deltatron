6c04c2cea4bd72c668dfa2c30dc375bb12ada00b
Tomislav Preksavec <tomislav.preksavec@gmail.com>
2016-12-16 15:23:28 +0100

    Added Slic3r config file

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -450,7 +450,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
   // these are the offsets to the probe relative to the extruder tip (Hotend - Probe)
   #define X_PROBE_OFFSET_FROM_EXTRUDER 11.5 
   #define Y_PROBE_OFFSET_FROM_EXTRUDER -6.0 
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -4.4  //Increase this if the first layer is too thin.
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -4.6  //Increase this if the first layer is too thin.
 
   #define Z_RAISE_BEFORE_HOMING 4       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
